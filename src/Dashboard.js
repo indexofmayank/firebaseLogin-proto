7b08544ed8f9a9e8e1d2e051422ee7425c3420b7
indexofmayank <mayankdevtiwari>
2023-04-03 03:22:05 +0530

    'registrationDone'

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState, useEffect, useSyncExternalStore } from "react";
+import { firebase } from "../config";
 
 const Dashboard = () => {
-  return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
-  )
-}
+  const [name, setName] = useState("");
 
-export default Dashboard
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setName(snapshot.data());
+        } else {
+          console.log("User does not exists");
+        }
+      });
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        Hello, {name.firstName}
+      </Text>
+      <TouchableOpacity
+        onPress={() => {firebase.auth().signOut()}}
+        style={styles.button}
+      >
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Sign out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default Dashboard;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+  },
+  textInput: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    width: 400,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#00e4d0",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: "#00e4d0",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+});
